3c5bf8654626d7352dfafb6230f4b1032823cf48
aishwariyaa.anand <aishwariyaa.anand@juspay.in>
2026-06-22 18:49:47 +0530

    fix sdk

--- sdk/javascript/src/payments/_generated_uniffi_client_flows.ts
+++ sdk/javascript/src/payments/_generated_uniffi_client_flows.ts
@@ -395,6 +395,23 @@ export class UniffiClient extends _UniffiClientBase {
     return this.callRes('post_authenticate', responseBytes, requestBytes, optionsBytes);
   }
 
+  /** Build connector HTTP request for post_risk_check flow. */
+  postRiskCheckReq(
+    requestBytes: Buffer | Uint8Array,
+    optionsBytes: Buffer | Uint8Array
+  ): Buffer {
+    return this.callReq('post_risk_check', requestBytes, optionsBytes);
+  }
+
+  /** Parse connector HTTP response for post_risk_check flow. */
+  postRiskCheckRes(
+    responseBytes: Buffer | Uint8Array,
+    requestBytes: Buffer | Uint8Array,
+    optionsBytes: Buffer | Uint8Array
+  ): Buffer {
+    return this.callRes('post_risk_check', responseBytes, requestBytes, optionsBytes);
+  }
+
   /** Build connector HTTP request for pre_authenticate flow. */
   preAuthenticateReq(
     requestBytes: Buffer | Uint8Array,
@@ -410,6 +427,23 @@ export class UniffiClient extends _UniffiClientBase {
     optionsBytes: Buffer | Uint8Array
   ): Buffer {
     return this.callRes('pre_authenticate', responseBytes, requestBytes, optionsBytes);
+  }
+
+  /** Build connector HTTP request for pre_risk_check flow. */
+  preRiskCheckReq(
+    requestBytes: Buffer | Uint8Array,
+    optionsBytes: Buffer | Uint8Array
+  ): Buffer {
+    return this.callReq('pre_risk_check', requestBytes, optionsBytes);
+  }
+
+  /** Parse connector HTTP response for pre_risk_check flow. */
+  preRiskCheckRes(
+    responseBytes: Buffer | Uint8Array,
+    requestBytes: Buffer | Uint8Array,
+    optionsBytes: Buffer | Uint8Array
+  ): Buffer {
+    return this.callRes('pre_risk_check', responseBytes, requestBytes, optionsBytes);
   }
 
   /** Build connector HTTP request for proxy_authorize flow. */
