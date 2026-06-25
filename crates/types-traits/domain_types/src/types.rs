01536871944a84fab15c952a1448fa9123803bb2
PiX <69745008+pixincreate@users.noreply.github.com>
2026-06-25 18:59:46 +0530

    feat(proto): migrate MandateAmountData.amount to Money type

--- crates/types-traits/domain_types/src/types.rs
+++ crates/types-traits/domain_types/src/types.rs
@@ -10819,6 +10819,7 @@ impl ForeignTryFrom<grpc_api_types::payments::SetupMandateDetails> for MandateDa
         value: grpc_api_types::payments::SetupMandateDetails,
     ) -> Result<Self, error_stack::Report<Self::Error>> {
         // Map the mandate_type from grpc type to domain type
+        #[allow(deprecated)]
         let mandate_type = value
             .mandate_type
             .and_then(|grpc_mandate_type| match grpc_mandate_type.mandate_type {
@@ -10826,9 +10827,9 @@ impl ForeignTryFrom<grpc_api_types::payments::SetupMandateDetails> for MandateDa
                     amount_data,
                 )) => Some(mandates::MandateDataType::SingleUse(
                     mandates::MandateAmountData {
-                        amount: common_utils::types::MinorUnit::new(amount_data.amount),
+                        amount: common_utils::types::MinorUnit::new(amount_data.amount_money.map(|m| m.minor_amount).unwrap_or(amount_data.amount)),
                         currency: grpc_api_types::payments::Currency::try_from(
-                            amount_data.currency,
+                            amount_data.amount_money.as_ref().map(|m| m.currency).unwrap_or(amount_data.currency),
                         )
                         .ok()
                         .and_then(|grpc_currency| {
@@ -10858,9 +10859,9 @@ impl ForeignTryFrom<grpc_api_types::payments::SetupMandateDetails> for MandateDa
                     amount_data,
                 )) => Some(mandates::MandateDataType::MultiUse(Some(
                     mandates::MandateAmountData {
-                        amount: common_utils::types::MinorUnit::new(amount_data.amount),
+                        amount: common_utils::types::MinorUnit::new(amount_data.amount_money.map(|m| m.minor_amount).unwrap_or(amount_data.amount)),
                         currency: grpc_api_types::payments::Currency::try_from(
-                            amount_data.currency,
+                            amount_data.amount_money.as_ref().map(|m| m.currency).unwrap_or(amount_data.currency),
                         )
                         .ok()
                         .and_then(|grpc_currency| {
