0ffead43c8d27ea4257e3740eaee9e6e82507461
Shubhodip pal <palshubhodip9@gmail.com>
2026-07-24 15:08:52 +0530

    fix(connector): Pass resource id field in Redsys Pre Authenticate response (#2000)

--- crates/types-traits/domain_types/src/types.rs
+++ crates/types-traits/domain_types/src/types.rs
@@ -16122,7 +16122,7 @@ pub fn generate_payment_authenticate_response<T: PaymentMethodDataTypes>(
                 .map(grpc_api_types::payments::PaymentStatus::foreign_from)
                 .unwrap_or_default();
             PaymentMethodAuthenticationServiceAuthenticateResponse {
-                connector_transaction_id: Some("session_created".to_string()),
+                connector_transaction_id: err.connector_transaction_id.clone(),
                 redirection_data: None,
                 network_transaction_id: None,
                 merchant_order_id: None,
@@ -16224,7 +16224,7 @@ pub fn generate_payment_post_authenticate_response<T: PaymentMethodDataTypes>(
                 .map(grpc_api_types::payments::PaymentStatus::foreign_from)
                 .unwrap_or_default();
             PaymentMethodAuthenticationServicePostAuthenticateResponse {
-                connector_transaction_id: None,
+                connector_transaction_id: err.connector_transaction_id.clone(),
                 redirection_data: None,
                 network_transaction_id: None,
                 merchant_order_id: None,
