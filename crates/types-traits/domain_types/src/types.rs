e5bc3dbc0a7fa1bbe8254be86ff0dd32f8ddcb99
Amitsingh Tanwar <126856945+AmitsinghTanwar007@users.noreply.github.com>
2026-08-03 21:05:36 +0530

    chore: add backward compatibility comments on merchant_*_id fields (#2048)

--- crates/types-traits/domain_types/src/types.rs
+++ crates/types-traits/domain_types/src/types.rs
@@ -6271,12 +6271,8 @@ pub fn generate_payment_authorize_response<T: PaymentMethodDataTypes>(
                     ),
                     network_transaction_id: network_txn_id,
                     connector_reference_id: connector_response_reference_id.clone(),
-                    merchant_transaction_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_transaction_id: connector_response_reference_id.clone(),
                     mandate_reference: mandate_reference_grpc,
                     mandate_reference_details,
                     incremental_authorization_allowed,
@@ -7281,13 +7277,9 @@ pub fn generate_payment_void_response(
                         &grpc_resource_id,
                     ),
                     status: grpc_status.into(),
-                    connector_reference_id: connector_response_reference_id,
-                    merchant_void_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    connector_reference_id: connector_response_reference_id.clone(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_void_id: connector_response_reference_id,
                     error: None,
                     status_code: u32::from(status_code),
                     response_headers: router_data_v2
@@ -7437,13 +7429,9 @@ pub fn generate_payment_void_post_capture_response(
                         &grpc_resource_id,
                     ),
                     status: grpc_status.into(),
-                    connector_reference_id: connector_response_reference_id,
-                    merchant_reverse_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    connector_reference_id: connector_response_reference_id.clone(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_reverse_id: connector_response_reference_id,
                     error: None,
                     status_code: u32::from(status_code),
                     response_headers: router_data_v2
@@ -7474,13 +7462,9 @@ pub fn generate_payment_void_post_capture_response(
                 Ok(PaymentServiceReverseResponse {
                     connector_transaction_id: connector_reference_id.clone().unwrap_or_default(),
                     status: grpc_status.into(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_reverse_id: connector_reference_id.clone(),
                     connector_reference_id,
-                    merchant_reverse_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
                     error: None,
                     status_code: u32::from(status_code),
                     response_headers: router_data_v2
@@ -7741,13 +7725,9 @@ pub fn generate_payment_sync_response(
                     connector_transaction_id: extract_connector_request_reference_id(
                         &grpc_resource_id,
                     ),
-                    connector_reference_id: connector_response_reference_id,
-                    merchant_transaction_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    connector_reference_id: connector_response_reference_id.clone(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_transaction_id: connector_response_reference_id,
                     redirection_data: redirection_data
                         .map(|form| grpc_api_types::payments::RedirectForm::foreign_try_from(*form))
                         .transpose()?,
@@ -7865,13 +7845,9 @@ pub fn generate_payment_sync_response(
                         .clone()
                         .map(ForeignFrom::foreign_from),
                     connector_transaction_id: resource_id.unwrap_or_default(),
-                    connector_reference_id: connector_response_reference_id,
-                    merchant_transaction_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    connector_reference_id: connector_response_reference_id.clone(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_transaction_id: connector_response_reference_id,
                     redirection_data: None,
                     status: grpc_status as i32,
                     mandate_reference: None,
@@ -8728,6 +8704,7 @@ pub fn generate_refund_sync_response(
                 connector_refund_id: response.connector_refund_id.clone(),
                 status: grpc_status as i32,
                 connector_reference_id: None,
+                // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
                 merchant_refund_id: Some(
                     router_data_v2
                         .resource_common_data
@@ -8875,8 +8852,9 @@ impl ForeignTryFrom<WebhookDetailsResponse> for PaymentServiceGetResponse {
                     .transpose()?
                     .unwrap_or_default(),
             ),
-            connector_reference_id: value.connector_response_reference_id,
-            merchant_transaction_id: None,
+            connector_reference_id: value.connector_response_reference_id.clone(),
+            // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+            merchant_transaction_id: value.connector_response_reference_id,
             status: status as i32,
             mandate_reference: mandate_reference_grpc,
             mandate_reference_details,
@@ -9137,6 +9115,7 @@ pub fn generate_void_post_refund_response(
                 connector_refund_id: response.connector_refund_id,
                 status: grpc_api_types::payments::RefundStatus::RefundSuccess as i32,
                 connector_reference_id: None,
+                // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
                 merchant_refund_id: Some(
                     router_data_v2
                         .resource_common_data
@@ -9477,8 +9456,9 @@ impl ForeignTryFrom<RefundWebhookDetailsResponse> for RefundResponse {
             connector_transaction_id: None,
             connector_refund_id: value.connector_refund_id.unwrap_or_default(),
             status: status.into(),
-            connector_reference_id: value.connector_response_reference_id,
-            merchant_refund_id: None,
+            connector_reference_id: value.connector_response_reference_id.clone(),
+            // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+            merchant_refund_id: value.connector_response_reference_id,
             merchant_transaction_id: value.merchant_transaction_id,
             error: Some(grpc_api_types::payments::ErrorInfo {
                 unified_details: None,
@@ -9550,8 +9530,9 @@ impl ForeignTryFrom<DisputeWebhookDetailsResponse> for DisputeResponse {
             evidence_documents: vec![],
             dispute_reason: None,
             dispute_message: value.dispute_message,
-            connector_reference_id: value.connector_response_reference_id,
-            merchant_dispute_id: None,
+            connector_reference_id: value.connector_response_reference_id.clone(),
+            // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+            merchant_dispute_id: value.connector_response_reference_id,
             status_code: value.status_code as u32,
             response_headers,
             raw_connector_request: None,
@@ -10174,6 +10155,7 @@ pub fn generate_refund_response(
                 connector_refund_id: response.connector_refund_id,
                 status: grpc_status as i32,
                 connector_reference_id: None,
+                // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
                 merchant_refund_id: Some(
                     router_data_v2
                         .resource_common_data
@@ -10756,13 +10738,9 @@ pub fn generate_payment_capture_response(
                     connector_transaction_id: extract_connector_request_reference_id(
                         &grpc_resource_id,
                     ),
-                    connector_reference_id: connector_response_reference_id,
-                    merchant_capture_id: Some(
-                        router_data_v2
-                            .resource_common_data
-                            .connector_request_reference_id
-                            .clone(),
-                    ),
+                    connector_reference_id: connector_response_reference_id.clone(),
+                    // Populated for backward compatibility; will be removed once Hyperswitch migrates to connector_reference_id
+                    merchant_capture_id: connector_response_reference_id,
                     error: None,
                     status: grpc_status.into(),
                     status_code: status_code as u32,
