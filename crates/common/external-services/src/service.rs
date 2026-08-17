52632aa33393ed867e34c1b714e421ce9e15da51
Tushar Shukla <tushar.shukla@juspay.in>
2026-08-18 00:11:05 +0530

    Revert "perf(core): stop cloning RouterDataV2 just to borrow it"
    This reverts commit 2b517bed36e02e5c3a7deb5735cf5f683dabbc57.

--- crates/common/external-services/src/service.rs
+++ crates/common/external-services/src/service.rs
@@ -659,7 +659,7 @@ where
         }
         (common_enums::CallConnectorAction::Trigger, TransportType::Http) => {
             let mut connector_request = connector
-                .build_request_v2(&router_data)
+                .build_request_v2(&router_data.clone())
                 .map_err(report_connector_request_to_flow)?;
 
             let mut updated_router_data = router_data.clone();
@@ -991,7 +991,7 @@ where
         }
         (common_enums::CallConnectorAction::Trigger, TransportType::Kafka) => {
             let kafka_record = connector
-                .build_kafka_record(&router_data)
+                .build_kafka_record(&router_data.clone())
                 .map_err(report_connector_request_to_flow)?;
 
             match kafka_record {
