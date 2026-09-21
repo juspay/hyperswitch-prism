401862581959509b5546afd005dd084960f4cc1e
Vani Gupta <118043711+Vani-1107@users.noreply.github.com>
2026-09-21 09:23:07 +0000

    feat(connector): [Revolv3] Implement Applepay and Googlepay - pre decrypt flows  (#2285)
    Co-authored-by: hyperswitch-bot[bot] <148525504+hyperswitch-bot[bot]@users.noreply.github.com>

--- crates/types-traits/domain_types/src/types.rs
+++ crates/types-traits/domain_types/src/types.rs
@@ -1825,6 +1825,7 @@ impl<
                                                 decrypted_data.application_expiration_year,
                                             )?,
                                             payment_data,
+                                            device_manufacturer_identifier: decrypted_data.device_manufacturer_identifier,
                                             merchant_token_identifier: decrypted_data
                                                 .merchant_token_identifier
                                                 .map(Secret::new),
